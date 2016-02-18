72f1c428c03896129f01c21ce1e85e92813faee0
Pete Brown <peter.brown@sage.com>
2016-02-18 15:20:40 +0000

    format attributes

--- lib/sageone_sdk/configurable.rb
+++ lib/sageone_sdk/configurable.rb
@@ -1,15 +1,8 @@
 module SageoneSdk
   # Configurable
   module Configurable
-    attr_accessor :access_token
-    attr_accessor :refresh_token
-    attr_accessor :connection_options
-    attr_accessor :default_media_type
-    attr_accessor :user_agent
-    attr_accessor :signing_secret
-    attr_accessor :redirect_uri
-    attr_accessor :client_id
-    attr_accessor :client_secret
+    attr_accessor :access_token, :refresh_token, :connection_options, :default_media_type,
+                  :user_agent, :signing_secret, :redirect_uri, :client_id, :client_secret
 
     attr_writer :api_endpoint
 
